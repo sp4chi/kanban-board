e9fbfbd8ce5d841443849797a64a14dbfe592d30
kaushik.bora <herock76@gmail.com>
2023-05-29 19:53:23 +0530

    -completed new ticket generation

--- app.js
+++ app.js
@@ -3,36 +3,39 @@
 const addBtn = document.querySelector('.add-btn');
 const removeBtn = document.querySelector('.remove-btn')
 const modal = document.querySelector('.modal-cont');
-const mainContainer = document.querySelector('.main-cont')
+const mainContainer = document.querySelector('.main-cont');
+const textArea = document.querySelector('.text-area-cont');
+const priorityColor = document.querySelectorAll('.priority-color');
 
 
 
 addBtn.addEventListener('click', function () {
     modal.classList.remove('hidden');
-
+    mainContainer.classList.add('hidden');
 });
 
 removeBtn.addEventListener('click', function () {
     modal.classList.add('hidden');
-
+    mainContainer.classList.remove('hidden');
 });
 
 
-const createTicker = function () {
+const createTicket = function () {
     const ticketContainer = document.createElement('div');
     ticketContainer.classList.add('ticket-cont');
     ticketContainer.innerHTML = `<div class="ticket-color"></div>
-    <div class="ticket-id">99999</div>
-    <div class="task-area">Fixing CSS bugs</div>`;
+    <div class="ticket-id"></div>
+    <div class="task-area"></div>`;
 
     mainContainer.appendChild(ticketContainer);
+    textArea.value = '';
+
+    modal.classList.add('hidden');
+    mainContainer.classList.remove('hidden');
 }
 
 modal.addEventListener('keydown', function (e) {
-    const key = e.key;
-    if (key === 'Shift') {
-        createTicker();
-
+    if (e.getModifierState('Shift')) {
+        createTicket();
     }
-
 })
