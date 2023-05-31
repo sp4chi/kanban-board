7d941917f0bee375db5098f366ea100cc04ceb44
kaushik.bora <herock76@gmail.com>
2023-06-01 04:30:17 +0530

    -app alerts user before deleting tickets

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
 'use-strict';
 
 const addBtn = document.querySelector('.add-btn');
-const removeBtn = document.querySelector('.remove-btn')
+const removeBtn = document.querySelector('.remove-btn');
 const modal = document.querySelector('.modal-cont');
 const mainContainer = document.querySelector('.main-cont');
 const textArea = document.querySelector('.text-area-cont');
 const priorityCardsModal = document.querySelectorAll('.priority-color');
 const priorityCardsToolbox = document.querySelectorAll('.color');
+const deleteBtn = document.querySelector('.remove-btn i');
+
+const settingDelete = function () {
+    setDelete = true;
+    deleteBtn.classList.add('delete');
+}
+
+const unsettingDelete = function () {
+    setDelete = false;
+    deleteBtn.classList.add('delete');
+}
 
 
-
-
-let allTickets;
+let tickets;
 let selectedCard = 'lightred';
-
-
+let setDelete = false;
 
 //OPEN - modal CLOSE - tickets
 addBtn.addEventListener('click', function () {
     modal.classList.remove('hidden');
     mainContainer.classList.add('hidden');
+    unsettingDelete();
 });
 
 //CLOSE - modal OPEN - tickets
 removeBtn.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    mainContainer.classList.remove('hidden');
+    if (setDelete === true) {
+        alert('Delete button has been activated!');
+        settingDelete();
+
+    } else {
+        modal.classList.add('hidden');
+        mainContainer.classList.remove('hidden');
+        settingDelete();
+        console.log('else is hit');
+    }
 });
 
 //make priority-cards inactive when not clicked
@@ -56,6 +73,8 @@ const priorityMessage = function () {
         return 'Closed';
 
 }
+
+//GENERATE NEW-TICKET 
 const createTicket = function () {
 
     const ticketContainer = document.createElement('div');
@@ -65,32 +84,44 @@ const createTicket = function () {
     <div class="task-area">${textArea.value}</div>`;
     ticketContainer.classList.add(`${selectedCard}-border`);
     mainContainer.appendChild(ticketContainer);
+    tickets = document.querySelectorAll('.ticket-cont');
     textArea.value = '';
-
-
 
     modal.classList.add('hidden');
     mainContainer.classList.remove('hidden');
 }
 
+//SHOW-ALL TICKETS
+const renderTickets = function () {
+    tickets.forEach((ticket) => {
+        ticket.classList.remove('hidden');
+    });
+}
+
+//ON PRESSING SHIFT NEW CARD IS SAVED, GENERATED AND RENDERED
 modal.addEventListener('keydown', function (e) {
     if (e.getModifierState('Shift')) {
         createTicket();
+        renderTickets();
+        settingDelete();
     }
 })
 
 //filter tickets by color
+const filterTickets = function (filterCardsColor) {
+
+    tickets.forEach((ticket) => {
+        ticket.classList.remove('hidden');
+        if (!ticket.classList[1].includes(filterCardsColor)) {
+            ticket.classList.add('hidden');
+        }
+    });
+};
+
+//ADDING-EVENT LISTENER TO ALL PRIORITY TOOLBOX CARDS
 priorityCardsToolbox.forEach((card) => {
     card.addEventListener('click', () => {
-        console.log(`${card.classList[0]} color has been selected!`);
-        const tickets = document.querySelectorAll('.ticket-cont');
-        tickets.forEach((ticket) => {
-            ticket.classList.remove('hidden');
-            if (!ticket.classList[1].includes(card.classList[0])) {
-                ticket.classList.add('hidden');
-            }
-        })
-
+        filterTickets(card.classList[0]);
     })
 })
 
